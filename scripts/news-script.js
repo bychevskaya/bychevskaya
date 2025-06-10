9ede111831ae3961087bff379066bf0fa2d25ec6
Ирина Бычевская <borovyhi@yandex.ru>
2025-06-10 17:35:58 +0500

    бутстрап+скрипт

---
+++ scripts/news-script.js
@@ -0,0 +1,42 @@
+﻿//// JavaScript source code
+//const news = [{
+//    name: 'Что смотреть в 2025 году: 50 самых ожидаемых зарубежных фильмов',
+//    link: 'https://www.kinopoisk.ru/media/article/4010470/'
+//},
+//{
+//    name: 'Все американские фильмы 2025 года',
+//    link: 'https://kino.mail.ru/cinema/all/2025/usa/'
+//},
+//{
+//    name: 'Премия Гильдии актеров — 2025: звезды на красной дорожке',
+//    link: 'https://www.kinopoisk.ru/media/article/4010721/'
+//},
+//{
+//    name: 'Российские актеры на «Оскаре 2025',
+//    link: 'https://kino.mail.ru/news/82874-anoriki-pokorili-gollivud-luchshie-momentyi-s-rossijskimi-akterami-na-oskare/'
+//},
+//{
+//    name: 'Два российских актера попали в лонг-лист британской кинопремии BAFTA',
+//    link: 'https://www.gazeta.ru/culture/news/2025/01/03/24766490.shtml'
+//},
+//];
+
+//function getLink(name, href) {
+//    let li = document.createElement('li');
+
+//    let a = document.createElement('a');
+//    a.textContent = name;
+//    a.setAttribute('href', href);
+
+//    li.append(a);
+//    return li;
+//}
+//function renderLinks(links, selector) {
+//    let container = document.querySelector(selector);
+
+//    links.forEach(link => {
+//        const element = getLink(link.name, link.link);
+//        container.append(element);
+//    });
+//}
+//renderLinks(news, 'section.list-group');
---
+++ scripts/script.js
@@ -0,0 +1,59 @@
+﻿// JavaScript source code
+"use strict";
+
+//const links = [
+//    {
+//        name: 'Главная',
+//        link: './index.html'
+//    },
+//    {
+//        name: 'Категории(+выпадающее меню)',
+//        link: './categories.html'
+//    },
+//    {
+//        name: 'Лучшие фильмы',
+//        link: './best-films.html'
+//    }
+//];
+
+const contactLinks = [{
+    name: 'Гитхаб',
+    link: 'https://github.com/bychevskaya'
+},
+{
+    name: 'Телеграм',
+    link: 'https://web.telegram.org/a/'
+},
+{
+    name: 'ВК',
+    link: 'https://vk.com/al_im.php?peers=410012921'
+},
+];
+function getLink(name, href) {
+    let li = document.createElement('li');
+
+    let a = document.createElement('a');
+    a.textContent = name;
+    a.setAttribute('href', href);
+
+    li.append(a);
+    return li;
+}
+function renderLinks(links, selector) {
+    let container = document.querySelector(selector);
+
+    links.forEach(link => {
+        const element = getLink(link.name, link.link);
+        container.append(element);
+    });
+
+}
+
+//renderLinks(links, 'ul.navigation');
+renderLinks(contactLinks, 'ol.navigation');
+
+
+//let navbar = document.querySelector('.navigation');
+//navbar.innerHTML = `
+//<li><a href="./categories.html">Категории(+выпадающее меню)</a></li>
+//                <li><a href="./best-films.html">Лучшие фильмы</a></li>`;
